a25c77db55f54ea4089d9ffc159d93218479c391
Anatoliy Dubovik <huykilik@gmail.com>
2022-06-11 15:30:28 +0300

    v001.02

--- restaurants/restaurants.go
+++ restaurants/restaurants.go
@@ -1,5 +1,11 @@
 package restaurants
 
+import (
+	"fmt"
+	"sort"
+	"strconv"
+)
+
 type Restaurant struct {
 	NameRestaurant    string
 	allTables         []int
@@ -49,3 +55,48 @@ var RestaurantMeatAndSalad = Restaurant{
 }
 
 var Restaurants = []Restaurant{RestaurantKaravella, RestaurantJuvenility, RestaurantMeatAndSalad}
+
+type ByWaitingForCooking []Restaurant
+
+func (a ByWaitingForCooking) Len() int { return len(a) }
+func (a ByWaitingForCooking) Less(i, j int) bool {
+	return float64(a[i].WaitingForCooking) < float64(a[j].WaitingForCooking)
+}
+func (a ByWaitingForCooking) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
+
+type ByAverageCheck []Restaurant
+
+func (a ByAverageCheck) Len() int           { return len(a) }
+func (a ByAverageCheck) Less(i, j int) bool { return a[i].AverageChek < a[j].AverageChek }
+func (a ByAverageCheck) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+
+func ActualReataurants(s string) []Restaurant {
+	p, err := strconv.Atoi(s)
+	if err != nil {
+		fmt.Println("Не корректный ввод")
+	}
+	var Restaurants01 = []Restaurant{RestaurantJuvenility, RestaurantKaravella, RestaurantMeatAndSalad}
+
+	sort.Sort(ByAverageCheck(Restaurants01))
+	sort.Sort(ByWaitingForCooking(Restaurants01))
+
+	var Restaurants02 = []Restaurant{}
+	for i := 0; i < len(Restaurants01); i++ {
+		if p <= 0 {
+			fmt.Println("Не верно указанно количество человек")
+			return nil
+		}
+		sum := 0
+		for q := 0; q < len(Restaurants01[i].FreeTables); q++ {
+			sum += Restaurants01[i].FreeTables[q]
+		}
+		if sum >= p {
+			fmt.Printf("\nРесторан: %s\nСвободно мест: %d\nВремя ожидания приготовления: %d\nСредний чек: %02.f\n", Restaurants01[i].NameRestaurant, sum, Restaurants01[i].WaitingForCooking, Restaurants01[i].AverageChek)
+			Restaurants02 = append(Restaurants02, Restaurants01[i])
+		}
+	}
+	if len(Restaurants02) <= 0 {
+		fmt.Println("Нет подходящих ресторанов для заявленного количества людей")
+	}
+	return Restaurants02
+}
